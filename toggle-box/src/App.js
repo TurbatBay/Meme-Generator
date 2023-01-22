13274ea4ba797b3c17c1ef469b7c7aecef143240
TurbatBay <learnwithturu@gmail.com>
2023-01-23 07:10:56 +0800

    tried to do day 44 panel

--- toggle-box/src/App.js
+++ toggle-box/src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import React from "react";
 import { useState } from "react";
 import BoxCont from "./components/BoxCont";
+import Accordian from "./components/Accordian";
 function App() {
   return (
     <div className="App">
-      <BoxCont />
+      {/* <BoxCont /> */}
+      <Accordian />
     </div>
   );
 }
