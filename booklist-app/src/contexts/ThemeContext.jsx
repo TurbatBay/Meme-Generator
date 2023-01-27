01e46a8e69c016e49f760fab9ae36e1bdaf65af3
TurbatBay <learnwithturu@gmail.com>
2023-01-28 06:40:54 +0800

    completed birthday reminder app

---
+++ booklist-app/src/contexts/ThemeContext.jsx
@@ -0,0 +1,9 @@
+// import React, { createContext } from "react";
+
+// export const ThemeContext = createContext();
+
+// function ThemeContextProvider() {
+//   return <ThemeContext.Provider value={{ ... }}></ThemeContext.Provider>;
+// }
+
+// export default ThemeContextProvider;
--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -2,31 +2,44 @@ import "./App.css";
 import peopleData from "./data/peopleData";
 import { useState } from "react";
 function App() {
-  const [card, setCard] = useState([]);
-  const pepData = peopleData.map((person) => (
-    <div className="card" key={person.id}>
-      <img className="images" src={person.img} alt={person.name} />
-      <div className="texts">
-        <h4>{person.name}</h4>
-        <h5>{person.age}</h5>
-      </div>
-      <button className="btn" onClick={() => removeClick(person.id)}>
-        Remove
-      </button>
-    </div>
-  ));
-  const removeClick = () => {
-    console.log("all cleared");
+  const [card, setCard] = useState(peopleData);
+
+  const removeClick = (id) => {
+    const filteredCard = card.filter((prevCard) => prevCard.id !== id);
+    setCard(filteredCard);
   };
-  const removeAllClick = (id) => {
-    console.log();
-  };
+
   return (
     <div className="App">
+      <h1>Birthday reminder App</h1>
+
       <div className="wrapper">
-        {pepData}
-        {/* <button onClick={() => removeAllClick(setCard([]))}>Remove All</button> */}
+        {card.map((person) => {
+          return (
+            <div className="card" key={person.id}>
+              <img className="images" src={person.img} alt={person.name} />
+              <div className="texts">
+                <h4>{person.name}</h4>
+                <h5>{person.age} years old</h5>
+              </div>
+              <button className="btn " onClick={() => removeClick(person.id)}>
+                Remove
+              </button>
+            </div>
+          );
+        })}
+
+        {card.length === 0 ? (
+          <button onClick={() => setCard(peopleData)} className="btn-all">
+            Show All
+          </button>
+        ) : (
+          <button onClick={() => setCard([])} className="btn-all">
+            Remove All
+          </button>
+        )}
       </div>
+      <p className="tag">Developed with passion by Turbat Bay</p>
     </div>
   );
 }
